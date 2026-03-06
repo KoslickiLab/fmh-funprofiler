168141955cf1fdef5b4cd466f62f4b951c4f53fe
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2026-03-06 14:29:44 +1100

    Rename package to 'fmhfunprofiler', no hyphen or underscore

--- fmh_funprofiler/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-fmh-funprofiler: Metagenomic functional profiler using FracMinHash sketches and KEGG KOs.
-"""
-
-__version__ = "1.1.0"
---
+++ fmhfunprofiler/__init__.py
@@ -0,0 +1,5 @@
+"""
+fmhfunprofiler: Metagenomic functional profiler using FracMinHash sketches and KEGG KOs.
+"""
+
+__version__ = "1.1.0"
